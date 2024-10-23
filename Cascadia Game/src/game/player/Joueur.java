54b66a656f9ce534dead9388d0e42da660f0a758
Tenseikage <113333861+Tenseikage@users.noreply.github.com>
2024-10-23 15:27:29 +0200

    Petite modif de la classe joueur

--- Cascadia Game/src/game/player/Joueur.java
+++ Cascadia Game/src/game/player/Joueur.java
@@ -3,11 +3,16 @@ import java.util.Objects;
 // Organisation en packages
 public record Joueur(String name, int age) {
 	public Joueur {
-			Objects.requireNonNull(name,"Nom de joueur inconnu !!!");
+		Objects.requireNonNull(name,"Nom de joueur inconnu !!!");
+		if (age < 0){
+			throw new IllegalArgumentException("Age Incorrect !!!");
+		}
 	}
 	
 	@Override
 	public String toString() {
 		return name + " " + age;
+		
+		  
 	}
 }
