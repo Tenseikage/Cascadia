3a4d521f3ffe9a039344c6d41451c4e253ff3600
Lowiq17 <lbergeot02@gmail.com>
2024-10-25 21:47:44 +0200

    Ajout de paramètre dans le record Joueur.java

--- Cascadia Game/src/game/player/Joueur.java
+++ Cascadia Game/src/game/player/Joueur.java
@@ -1,7 +1,8 @@
 package game.player;
+
 import java.util.Objects;
-// Organisation en packages
-public record Joueur(String name, int age) {
+
+public record Joueur(String name, int age, int nomberOfTiles, int points) {
 	public Joueur {
 		Objects.requireNonNull(name,"Nom de joueur inconnu !!!");
 		if (age < 0){
@@ -11,7 +12,7 @@ public record Joueur(String name, int age) {
 	
 	@Override
 	public String toString() {
-		return name + " " + age;
+		return "<"+name+">" + " " + age + " " + "";
 		
 		  
 	}
