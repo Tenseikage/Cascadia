d7ccbb84c3ef92c788bb2cfe035f6f66681c25e2
Lowiq17 <lbergeot02@gmail.com>
2025-01-12 02:02:59 +0100

    test build.xml

--- Game/docs/doc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"game.display"},{"l":"game.logic"},{"l":"game.main"},{"l":"game.material"},{"l":"game.player"}];updateSearchResults();
